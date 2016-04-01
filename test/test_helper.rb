bcaadecf502c283a07e78abcbe2aacb3a4055cef
Zeroalpha <christian.moniatis@gmail.com>
2016-04-02 00:47:05 +0200

    Added character maps and basic test data

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,28 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'zconv'
 
 require 'minitest/autorun'
+
+module TestData
+  #TestData::TARGET
+  lorem_ipsum = %q{
+Lorem ipsum dolor sit amet, mei ad exerci possim omnium, id vis amet interpretaris. Vel eius aeque accusata no. Eu dico consulatu vim, scripta feugiat admodum mel ne, ius te enim exerci delenit. Prima omittam eum te, probo ubique veritus in cum. Et hinc fuisset volutpat quo.
+
+Mundi corpora an vim. Erant vitae sit ut, mazim epicurei ut eum, ex putant feugait reprimique nec. Inani feugait repudiare at sit, ut mundi ridens vis. Vim ornatus mandamus petentium et, cu eos agam necessitatibus. Quo eu atqui putent fastidii.
+}
+
+  targets = {}
+  targets[:fb80] = lorem_ipsum.lines.map do |line|
+    line.chomp!
+    ret = line.slice!(0,80) << "\n"
+    ret << line.slice!(0,80) << "\n" until line.empty?
+    ret
+  end.join("\n")
+
+  const_set 'TARGET', targets
+
+  #TestData::SOURCE
+  sources = {
+    fb80: 
+  }
+
+end
